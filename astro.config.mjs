d8a349fb392fe0400d74ff048227a52ea66edda6
Amir Aziz <amir.zezo2001@gmail.com>
2025-05-02 17:09:40 +0500

    fix prose

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import markdoc from '@astrojs/markdoc'
-import keystatic from '@keystatic/astro'
-import tailwindcss from '@tailwindcss/vite'
-import partytown from '@astrojs/partytown'
-import sitemap from '@astrojs/sitemap'
-import svelte from '@astrojs/svelte'
-import netlify from '@astrojs/netlify'
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), partytown(), sitemap(), svelte()],
+  integrations: [
+    react(),
+    markdoc(),
+    keystatic(),
+    partytown(),
+    sitemap(),
+    svelte(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server',
+  output: "static",
   adapter: netlify(),
-})
+});
